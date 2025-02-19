a07f06cd67ec0611ae2a654e8325250ca2edbc7a
moideenshacp <153590227+moideenshacp@users.noreply.github.com>
2025-02-19 10:03:11 +0530

    configured frontend socket

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_CHAT_SERVER, {
   withCredentials: true,
+  transports: ["websocket"],
+  secure: true, 
 });
 
 export default socket;
